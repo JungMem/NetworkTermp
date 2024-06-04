97b4d66c0147549b1241ec5912686d81a97bdd46
m0ng <108099255+JungMem@users.noreply.github.com>
2024-06-04 22:18:16 +0900

    Update client.c

--- client.c
+++ client.c
@@ -2,216 +2,158 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <malloc.h>
-#include <pthread.h>
 
-#include <sys/select.h>
-#include <sys/time.h>
-#include <signal.h>
-#include <errno.h>
-
-void stopInt(int sig){
-    signal(sig, SIG_IGN);
-}
-
-// Define Max Client Num
-#define MAX_CLIENT 10
 // One byte for Null Termination
 #define BUF_SIZE 1024+1
 
-pthread_mutex_t mutex;
+unsigned int countSp(const char *str, unsigned int *ptr) {
 
-//////////////////// KVS STRUCT & KVS FUNC ////////////////////
-unsigned int kvs_cnt = 0;
-struct kvs{
-    char* key;
-    char* value;
-};
-struct kvs KVS[10];
+    // var For check ptr's idx
+    unsigned int countNum = 0;
+    // var For check sp's place
+    unsigned int countPtr = 0;
 
-void KVSinit(){
+    while (*str != '\0') {
 
-    int i;
-    for(i = 0; i < 10; i++){
-        memset(&KVS[i], 0, sizeof(struct kvs));
-    }
-}
+        if (*str == ' ') {
 
-void KVSfin(){
-
-    int i;
-    for(i = 0; i < kvs_cnt; i++){
-        free(KVS[i].key);
-        free(KVS[i].value);
-    }
-}
-
-int KVSfind(char* key){
-
-    int i;
-    for(i = 0; i < kvs_cnt; i++){
-        if(strncmp(KVS[i].key, key, strlen(key))==0){
-            return i;
+            if(countNum < 2) ptr[countNum] = countPtr;   
+            countNum++;
         }
+        countPtr++;
+        str++;
     }
-    return -1;
+    return countNum;
 }
 
-void KVSset(char *key, char *value){
 
-    int idx = KVSfind(key);
-    if(idx != -1){
-        free(KVS[idx].value);
-        KVS[idx].value = value;
+
+
+//////////////////// Transform ASCII TO RESP2 or RESP2 TO ASCII FUNC ////////////////////
+
+// return -1: Error
+// return 0: EXIT
+// return 1: GET || SET
+int transAscii(char* buf, char* comp){
+
+    unsigned int ptr[3];
+
+    // Process Set Command
+    if(strncmp(buf, "set", 3)==0){
+
+        if(countSp(buf, ptr) != 2){
+            printf("(error) ERR wrong number of arguments for 'set' command\n");
+            return -1;
+        }
+
+        // Ex: set(ptr[0])key(ptr[1])value(ptr[2])
+        ptr[2] = strlen(buf);
+
+        // Slice Strings
+        // Ex: set(\0)key(\0)value\0
+        int i;
+        for(i = 0; i < 2; i++) buf[ptr[i]] = '\0';
+
+        strncpy(comp, "*3\r\n$3\r\nSET\r\n\0", 14);
+        sprintf(comp+strlen(comp), "$%d\r\n%s\r\n\0", ptr[1]-ptr[0]-1, buf+ptr[0]+1);
+        sprintf(comp+strlen(comp), "$%d\r\n%s\r\n\0", ptr[2]-ptr[1]-1, buf+ptr[1]+1);
+
+        return 1;
+
     }
+    // Process Get Command
+    if(strncmp(buf, "get", 3)==0){
+
+        if(countSp(buf, ptr) != 1){
+            printf("(error) ERR wrong number of arguments for 'get' command\n");
+            return -1;
+        }
+
+        // Ex: get(ptr[0])key(ptr[1])
+        ptr[1] = strlen(buf);
+
+        // Slice Strings
+        // Ex: get(\0)key\0
+        buf[ptr[0]] = '\0';
+
+        strncpy(comp, "*2\r\n$3\r\nGET\r\n\0", 14);
+        sprintf(comp+strlen(comp), "$%d\r\n%s\r\n\0", ptr[1]-ptr[0]-1, buf+ptr[0]+1);
+
+        return 1;
+
+    }
+    // Process EXIT Command
+    else if(strncmp(buf, "EXIT\0", sizeof(5))==0){
+
+        strncpy(comp, "*1\r\n$4\r\nQUIT\r\n\0", 15);
+        return 0;
+    }
+    // Process Error Exception
     else{
-        KVS[kvs_cnt].key = key;
-        KVS[kvs_cnt].value = value;
-        kvs_cnt++;
+        printf("(error) ERR unknown command '%s'\n", buf);
+        return -1;
     }
+
 }
 
-char* KVSget(char* key){
+// return -1: ERROR
+// return 0: Simple Strings
+// return 1: Bulk Strings
+int parseResp(char* buf){
 
-    int idx = KVSfind(key);
-    if(idx != -1){
-        char* tmp = KVS[idx].value;
-        return tmp;
-    }
-    else{
-        return NULL;
-    }
-}
-///////////////////////////////////////////////////////////////
+    // Parse Bulk Strings
+    if(*buf == '$'){
 
-
-
-//////////////////// RESP Parsing FUNC ////////////////////
-char* parseResp(char* buf){
-
-    // Extract NUM to num
-    // Ex: $(((NUM)))\r\nStrings\r\n
-    unsigned int num = atoi(buf+1);
-
-    // Mov buf to Strings
-    // Ex: $NUM\r\nStrings\r\n => Strings\r\n
-    while(*buf != '\r') buf++;
-    buf += 2;
-
-    // Extract Strings to extr
-    // Ex: (((Strings)))\r\n
-    char* extr = (char*)malloc(num+1);  // For Null Termination
-    memset(extr, 0, num+1);             // For Null Termination
-    strncpy(extr, buf, num);
-
-    // return Strings' Pointer
-    return extr;
-}
-///////////////////////////////////////////////////////////
-
-
-
-//////////////////// Client Interaction FUNC ////////////////////
-void* cliInteract(void* clientSocket){
-
-    int soc = *(int*)clientSocket;
-    printf("%d Socket connected\n", soc);
-
-    int res;
-    char buf[BUF_SIZE];
-    char* key;
-    char* value;
-    char* tmp;
-
-    for(;;){
-
-        res = recv(soc, buf, BUF_SIZE, 0);
-        if(res <= 0){
-            perror("***recv API error***\n");
-            close(soc);
-            exit(1);
+        if(strncmp(buf, "$-1", 3) == 0){
+            printf("(nil)\n");
+            return 1;
         }
-        buf[res] = '\0';
-
-        // Process SET Command
-        if(strncmp(buf, "*3\r\n$3\r\nSET\r\n", 13)==0){
-
-            // Mov Buf to First String
-            tmp = buf+13;
-
-            key = parseResp(tmp);
-            
-            // Mov Buf to Second String
-            tmp++;
-            while(*tmp != '$') tmp++;
-            
-            value = parseResp(tmp);
-
-            pthread_mutex_lock(&mutex);
-            KVSset(key, value);
-            pthread_mutex_unlock(&mutex);
-            
-            strncpy(buf, "+OK\r\n\0", 6);
-        }
-        // Process GET Command
-        else if(strncmp(buf, "*2\r\n$3\r\nGET\r\n", 13)==0){
-
-            key = parseResp(buf+13);
-
-            pthread_mutex_lock(&mutex);
-            value = KVSget(key);
-            pthread_mutex_unlock(&mutex);
-
-            if(value != NULL){
-                sprintf(buf, "$%ld\r\n%s\r\n\0", strlen(value), value);
-            }
-            else{
-                strncpy(buf, "$-1\r\n\0", 5);
-            }
-        }
-        // Process EXIT Command
-        else if(strncmp(buf, "*1\r\n$4\r\nQUIT\r\n", 14)==0){
-
-            res = send(soc, "+OK\r\n", 5, 0);
-            if(res <= 0){
-                perror("***send API error***\n");
-                close(soc);
-                exit(1);
-            }
-            break;
-        }
-        // Process Error
         else{
-            strncpy(buf, "-ERR\r\n\0", 7);
-        }
 
-        res = send(soc, buf, strlen(buf), 0);
-        if(res <= 0){
-            perror("***send API error***\n");
-            close(soc);
-            exit(1);
+            // Ex: $4\r\ntest\r\n => test\r\n
+            while(*buf != '\r') buf++;
+            buf += 2;
+
+            // backup To print out
+            char* backup = buf;
+
+            // Ex: test\r\n => \r\n
+            while(*buf != '\r') buf++;
+            
+            // Ex: $4\r\ntest\r\n => $4\r\ntest\0\n
+            *buf = '\0';
+
+            printf("\"%s\"\n", backup);
+
+            return 1;
         }
 
     }
-
-    close(soc);
-    printf("%d Socket disconnected\n", soc);
+    // Parse Simple Strings
+    else if(strncmp(buf, "+OK\r\n", 5)==0){
+        printf("OK\n");
+        return 0;
+    }
+    // Process Error Exeception
+    else{
+        printf("***Unintelligible Response from Server***\n");
+        return -1;
+    }
 
 }
-/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-//////////////////// Process Socket FUNC ////////////////////
-int main(int argc, char *argv[]){
+//////////////////// Process Socket and Server Interaction FUNC ////////////////////
+int main(int argc, char* argv[]){
 
-    KVSinit();
-
-    int serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(serverSocket < 0){
-        perror("***socket API error ***\n");
+    // Process Socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(clientSocket < 0){
+        perror("***socket API error***\n");
         return -1;
     }
 
@@ -219,90 +161,64 @@ int main(int argc, char *argv[]){
     memset(&sockStruct, 0, sizeof(struct sockaddr_in));
 
     sockStruct.sin_family = AF_INET;
-    sockStruct.sin_port = htons(atoi(argv[1]));
-    sockStruct.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockStruct.sin_port = htons(atoi(argv[2]));
 
-    if(bind(serverSocket, (struct sockaddr *)&sockStruct, sizeof(sockStruct)) < 0){
-        perror("***bind API error***\n");
-        close(serverSocket);
+    if(inet_pton(AF_INET, argv[1], &sockStruct.sin_addr.s_addr) != 1){
+        perror("***inet_pton API error***\n");
+        close(clientSocket);
         return -1;
     }
 
-    if(listen(serverSocket, MAX_CLIENT) < 0){
-        perror("***listen API error***\n");
-        close(serverSocket);
+    if(connect(clientSocket, (struct sockaddr *)&sockStruct, sizeof(sockStruct)) < 0){
+        perror("***connect API error***\n");
+        close(clientSocket);
         return -1;
     }
 
-    int threadHandle;
-    
-    int res;
-    fd_set reads, temps;
-    struct timeval timeout;
-
-    FD_ZERO(&reads);
-    FD_SET(serverSocket, &reads);
-
-    signal(SIGINT, stopInt);
-    printf("Ctrl + C for Stop Program\n");
-
-    pthread_mutex_init(&mutex, NULL);
+    // Server Interaction
     for(;;){
 
-        temps = reads;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 500;
+        int res = 0;
 
-        res = select(serverSocket+1, &temps, 0, 0, &timeout);
-        if(res < 0){
+        // flag For transAscii
+        int flag1 = 0;
+        // flag For parseResp
+        int flag2 = 0;
+        
+        char buf[BUF_SIZE];
+        char comp[BUF_SIZE];
 
-            // ctrl + c
-            if(errno == EINTR){
-                break;
-            }
-            // Error
-            else{
-                perror("***select API error***\n");
-                close(serverSocket);
-                return -1;
-            }
+        fgets(buf, BUF_SIZE, stdin);
+        buf[strcspn(buf, "\n")] = '\0';
+
+        flag1 = transAscii(buf, comp);
+        if(flag1 == -1) continue;
+
+        res = send(clientSocket, comp, strlen(comp), 0);
+        if(res <= 0){
+            perror("***send API error***\n");
+            close(clientSocket);
+            return -1;
         }
-        // timeout
-        else if(res == 0){
-            continue;
+
+        res = recv(clientSocket, buf, BUF_SIZE, 0);
+        if(res <= 0){
+            perror("***recv API error***\n");
+            close(clientSocket);
+            return -1;
         }
-        else{
-            if(FD_ISSET(serverSocket, &temps)){
+        buf[res] = '\0';
 
-                int clientSocket = accept(serverSocket, NULL, NULL);
-                if(clientSocket < 0){
-                    perror("***accept API error***\n");
-                    close(serverSocket);
-                    return -1;
-                }
-
-                pthread_t thread;
-                if(pthread_create(&thread, NULL, cliInteract, (void*)&clientSocket) < 0){
-                    perror("***pthread_create API error***\n");
-                    close(serverSocket);
-                    return -1;
-                }
-
-                if(pthread_detach(thread) != 0){
-                    perror("***pthread_detach API error***\n");
-                    close(serverSocket);
-                    return -1;
-                }
-            }
+        flag2 = parseResp(buf);
+        // ERROR or EXIT
+        if(flag2 == -1 || (flag1 == 0 && flag2 == 0)){
+            break;
         }
 
     }
-    pthread_mutex_destroy(&mutex);
-    
-    close(serverSocket);
-    KVSfin();
 
+    close(clientSocket);
     return 0;
 
 }
-/////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
